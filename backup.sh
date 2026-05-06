d323083ca23fb8434b8346e108a326abaea2d15e
S.Hag <SMan@mailbox.org>
2026-05-06 07:47:57 +0200

    chk

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Convenience script to backup VSCodium settings
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 "$DIR/VSCodiumSync.sh" --action Backup --backup-path "$DIR/VSCodium-Backup"
--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Convenience script to restore VSCodium settings
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 "$DIR/VSCodiumSync.sh" --action Restore --backup-path "$DIR/VSCodium-Backup"
